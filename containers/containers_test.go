379da9590a5e4f7b4ce265f774ecd97efef67abc
Mathieu Lecarme <mlecarme@bearstech.com>
2018-05-25 16:39:05 +0200

    More tests

--- containers/containers_test.go
+++ containers/containers_test.go
@@ -26,7 +26,13 @@ func (t *TestProcess) Executable() string {
 	return t.exe
 }
 
-func TestContainers(t *testing.T) {
+func TestRegexp(t *testing.T) {
+	s := PatternDockerID.FindStringSubmatch("docker-containerd-shim -namespace moby -workdir /var/lib/docker/containerd/daemon/io.containerd.runtime.v1.linux/moby/28e4253a8a17f2bf9cf7ff44546f90134ade2106a23a4361f0993872f3fbf398 -address /var/run/docker/containerd/docker-containerd.sock -containerd-binary /usr/bin/docker-containerd -runtime-root /var/run/docker/runtime-runc")
+	assert.Len(t, s, 2)
+	assert.Equal(t, "28e4253a8a17f2bf9cf7ff44546f90134ade2106a23a4361f0993872f3fbf398", s[1])
+}
+
+func _TestContainers(t *testing.T) {
 	procs := []ps.Process{
 		&TestProcess{
 			pid: 23940,
